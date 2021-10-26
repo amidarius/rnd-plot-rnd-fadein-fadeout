6dd39e98a76f2ff1786585f9ff72a59793887111
amidarius <amidarius@yahoo.com>
2021-10-26 12:34:45 +0200

    Intenzitet je sada 2^rnd(1-8)-1

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     for (let index = 0; index < 255; index++) {
-        led.plotBrightness(randint(0, 4), randint(0, 4), randint(0, 32) * 8)
+        led.plotBrightness(randint(0, 4), randint(0, 4), 2 ** randint(1, 8) - 1)
         led.plotBrightness(randint(0, 4), randint(0, 4), 0)
-        basic.pause(20)
+        basic.pause(10)
     }
 })
